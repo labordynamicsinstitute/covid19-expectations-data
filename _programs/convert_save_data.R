779e9ae8bc3d63acbe748334017bcaa48fa00f77
Lars Vilhuber <lars@vilhuber.com>
2020-05-27 22:34:35 -0400

    Switching save mode to Stata version 14

--- _programs/convert_save_data.R
+++ _programs/convert_save_data.R
@@ -5,7 +5,7 @@
 # write out R file
 saveRDS(results.final,file= paste0(derivedfile,".Rds"))
 # output Stata file
-write_dta(results.final,path = paste0(derivedfile,".dta"),version = 15,
+write_dta(results.final,path = paste0(derivedfile,".dta"),version = 14,
           label =paste0("Expectations ",Sys.Date()))
 # write out CSV file
 write_csv(results.final,path= paste0(derivedfile,".csv"))
